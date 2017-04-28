b86643d92efb2ece0504cb73cb9716f6f3eb20e2
robbykraft <robbykraft@gmail.com>
2017-04-28 10:15:26 -0400

    PDF format lesson, activity view share or print

--- Character/Lessons/Pages/LessonViewController.swift
+++ Character/Lessons/Pages/LessonViewController.swift
@@ -203,10 +203,103 @@ class LessonViewController: UIViewController, CompletedQuestionDelegate, MyNotes
 			self.navigationController?.present(vc, animated: true, completion:nil)
 		}))
 		alert.addAction(UIAlertAction(title: "Print", style: .default , handler:{ (UIAlertAction)in
-			
+			self.createPDF()
+			self.sharePDFFile()
 		}))
 		alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel , handler:nil))
 		self.present(alert, animated: true) { }
+	}
+	
+	
+	
+	func createPDF(){
+		let fileName = "lesson.pdf"
+		let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+		let pdfFileName = path.appending(fileName)
+		
+		do {
+			try FileManager.default.removeItem(atPath: pdfFileName)
+		} catch let error as NSError {
+			print(error.debugDescription)
+		}
+		
+		let cfTitleText = titleLabel.text! as CFString
+		let cfBodyText = bodyText.text! as CFString
+		// Prepare the text using a Core Text Framesetter.
+		
+		// create a dictionary of attributes to be applied to the string
+		let titleOptions:CFDictionary = [NSFontAttributeName:UIFont(name: SYSTEM_FONT, size: 30)!] as CFDictionary
+		let bodyOptions:CFDictionary = [NSFontAttributeName:UIFont(name: SYSTEM_FONT, size: 20)!] as CFDictionary
+		
+		let bodyString = CFAttributedStringCreate(nil, cfBodyText, bodyOptions)!
+		let titleString = CFAttributedStringCreate(nil, cfTitleText, titleOptions)!
+
+		let framesetterBody = CTFramesetterCreateWithAttributedString(bodyString)
+		let framesetterTitle = CTFramesetterCreateWithAttributedString(titleString)
+		let bodyFrameRect = CGRect(x: 30, y: 0, width: 612-60, height: 792-60)
+		let bodyFramePath = CGPath.init(rect: bodyFrameRect, transform: nil)
+		let titleFrameRect = CGRect(x: 30, y: 0, width: 612-60, height: 792-60)
+		let titleFramePath = CGPath.init(rect: titleFrameRect, transform: nil)
+		// Get the frame that will do the rendering.
+		let currentRange = CFRange(location: 0, length: 0)
+		let bodyFrameRef = CTFramesetterCreateFrame(framesetterBody, currentRange, bodyFramePath, nil)
+		let titleFrameRef = CTFramesetterCreateFrame(framesetterTitle, currentRange, titleFramePath, nil)
+
+		// Create the PDF context using the default page size of 612 x 792.
+		UIGraphicsBeginPDFContextToFile(pdfFileName, CGRect.zero, nil)
+		// Mark the beginning of a new page.
+		UIGraphicsBeginPDFPageWithInfo(CGRect(x:0, y:0, width:612, height:792), nil)
+		// Get the graphics context.
+		let currentContext = UIGraphicsGetCurrentContext()
+		currentContext!.textMatrix = CGAffineTransform.identity
+		// Core Text draws from the bottom-left corner up, so flip
+		// the current transform prior to drawing.
+		currentContext!.scaleBy(x: 1.0, y: -1.0)
+		currentContext!.translateBy(x: 0, y: -792)
+		CTFrameDraw(titleFrameRef, currentContext!)
+		currentContext!.translateBy(x: 0, y: -60)
+//		 Draw the frame.
+		CTFrameDraw(bodyFrameRef, currentContext!)
+//		 Close the PDF context and write the contents out.
+		UIGraphicsEndPDFContext();
+	}
+	
+	func sharePDFFile(){
+		let fileName = "lesson.pdf"
+		let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+		let pdfFileName = path.appending(fileName)
+		let objectsToShare = [pdfFileName];
+		
+//		let types:[UIActivityType] = [.print, .airDrop, .mail, .message, .openInIBooks, .addToReadingList]
+		let controller:UIActivityViewController = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+		controller.excludedActivityTypes = [.postToFacebook, .postToVimeo, .postToWeibo, .postToFlickr, .postToTwitter]
+		
+		// Exclude activities
+//		NSArray *excludedActivities = @[UIActivityTypePostToTwitter, UIActivityTypePostToFacebook,
+//		UIActivityTypePostToWeibo,
+//		UIActivityTypeMessage, UIActivityTypeMail,
+//		UIActivityTypePrint, UIActivityTypeCopyToPasteboard,
+//		UIActivityTypeAssignToContact, UIActivityTypeSaveToCameraRoll,
+//		UIActivityTypeAddToReadingList, UIActivityTypePostToFlickr,
+//		UIActivityTypePostToVimeo, UIActivityTypePostToTencentWeibo];
+//		controller.excludedActivityTypes = excludedActivities;
+		
+		self.present(controller, animated: true, completion: nil)
+	}
+	
+	
+	func showPDFFile(){
+		let fileName = "lesson.pdf"
+		let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+		let pdfFileName = path.appending(fileName)
+	
+		let webView = UIWebView.init(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+		let url:URL = URL(fileURLWithPath: pdfFileName)
+		let request:URLRequest = URLRequest(url: url)
+		webView.scalesPageToFit = true
+		webView.loadRequest(request)
+		
+		self.view.addSubview(webView)
 	}
 
 }
---
+++ Character/Singletons/PDFCreator.swift
@@ -0,0 +1,14 @@
+//
+//  PDFCreator.swift
+//  Character
+//
+//  Created by Robby on 4/28/17.
+//  Copyright © 2017 Robby. All rights reserved.
+//
+
+import UIKit
+
+class PDFCreator: NSObject {
+
+	
+}
