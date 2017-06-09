e1a2a5b357367d860ffe1a378656887d9c47d8f7
robbykraft <robbykraft@gmail.com>
2017-06-09 13:53:39 -0400

    all website material

---
+++ web/admin/dist/js/fireuser.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyD4Ioz8DP3mOxwLSMMogv3O9Mi5HKn8mLQ",
+    authDomain: "character-68dcf.firebaseapp.com",
+    databaseURL: "https://character-68dcf.firebaseio.com",
+    storageBucket: "character-68dcf.appspot.com",
+    messagingSenderId: "614929615875"
+};
+firebase.initializeApp(config);
