7fd7007d0a803521377ab5813662accd021c8394
robbykraft <robbykraft@gmail.com>
2018-01-03 11:53:37 -0500

    convert to dictionary and back

--- Pollen/iOS/Allergy/Model/Allergy.swift
+++ Pollen/iOS/Allergy/Model/Allergy.swift
@@ -110,21 +110,21 @@ enum AllergyConditions:String {
 }
 
 
-enum eyes_allergies{ case itchy, red, watery, bags }
-enum sinus_allergies { case pressure, pain, colored_discharge, tooth_pain }
-enum nose_allergies { case stuffy, itchy, runny, blood }
-enum throat_allergies { case clearing, sore, hoarse_voice, itchy_scratchy }
-enum lungs_allergies { case wheeze, cough, tight_chest, short_breath }
-enum skin_allergies { case itch, bumps_hives, eczema, swelling }
-enum meds_allergies { case pill, nose_spray, inhaler, topical_cream }
-enum wokeUp_allergies { case groggy_head, stuffed_up, crusty_eyes, dry_lips }
-enum nightTime_allergies { case stuffy, cough, throat_drainage, wheeze_chest }
-enum pet_allergies { case dog, cat, rodent, horse }
-enum mold_allergies { case home, work, school, other }
-enum dust_allergies { case home, work, school, other }
-enum outdoors_allergies { case playing_sports, air_travel, someone_sick, air_pollution }
-enum energyLevel_allergies { case great, medium, low, sick }
-enum headache_allergies { case forehead, sides, back, behind_eyes }
+enum eyes_allergies:String{ case itchy, red, watery, bags }
+enum sinus_allergies:String { case pressure, pain, colored_discharge, tooth_pain }
+enum nose_allergies:String { case stuffy, itchy, runny, blood }
+enum throat_allergies:String { case clearing, sore, hoarse_voice, itchy_scratchy }
+enum lungs_allergies:String { case wheeze, cough, tight_chest, short_breath }
+enum skin_allergies:String { case itch, bumps_hives, eczema, swelling }
+enum meds_allergies:String { case pill, nose_spray, inhaler, topical_cream }
+enum wokeUp_allergies:String { case groggy_head, stuffed_up, crusty_eyes, dry_lips }
+enum nightTime_allergies:String { case stuffy, cough, throat_drainage, wheeze_chest }
+enum pet_allergies:String { case dog, cat, rodent, horse }
+enum mold_allergies:String { case home, work, school, other }
+enum dust_allergies:String { case home, work, school, other }
+enum outdoors_allergies:String { case playing_sports, air_travel, someone_sick, air_pollution }
+enum energyLevel_allergies:String { case great, medium, low, sick }
+enum headache_allergies:String { case forehead, sides, back, behind_eyes }
 
 struct Allergies{
 	var eyes:[eyes_allergies]?
@@ -155,45 +155,69 @@ func allAllergyConditions() -> [AllergyConditions]{
 	//	return results
 }
 
-func infrequentCharacterRemoval(_ paragraph:String) -> [Character]{
-	let max = 50
-	var dict:[Character:Int] = [:]
-	let total = paragraph.count
-	// count every character
-	for letter in paragraph{
-		if let entry = dict[letter]{
-			dict[letter] = entry + 1
-		} else{
-			dict[letter] = 1
-		}
-	}
-	
-	let sortedKeys = Array(dict.keys).sorted { (a, b) -> Bool in
-		return dict[a]! < dict[b]!
-	}
-	
-	var cumulativeSum:Int = 0
-	let cumulative = sortedKeys.map { (letter) -> Int in
-		cumulativeSum += dict[letter] ?? 0
-		return cumulativeSum
-	}.map{
-		total - $0
-	}.filter{
-		$0 > max
-	}
-	return Array(sortedKeys.prefix(cumulative.count))
-}
-
-
 class AllergyEntry: NSObject {
 
-	var date:Date?
+	var date:Date
 	var allergies:Allergies?
 	
-	func dictionary() -> [String:Any]{
-		
-		return []
+	func dictionary() -> [String:[String]]{
+		var d:[String:[String]] = [:]
+		if let a = allergies{
+			if let y = a.eyes{ d["eyes"]=[String](); for z in y{d["eyes"]!.append(z.rawValue)} }
+			if let y = a.sinus{ d["sinus"]=[String](); for z in y{d["sinus"]!.append(z.rawValue)} }
+			if let y = a.nose{ d["nose"]=[String](); for z in y{d["nose"]!.append(z.rawValue)} }
+			if let y = a.throat{ d["throat"]=[String](); for z in y{d["throat"]!.append(z.rawValue)} }
+			if let y = a.lungs{ d["lungs"]=[String](); for z in y{d["lungs"]!.append(z.rawValue)} }
+			if let y = a.skin{ d["skin"]=[String](); for z in y{d["skin"]!.append(z.rawValue)} }
+			if let y = a.meds{ d["meds"]=[String](); for z in y{d["meds"]!.append(z.rawValue)} }
+			if let y = a.wokeUp{ d["wokeUp"]=[String](); for z in y{d["wokeUp"]!.append(z.rawValue)} }
+			if let y = a.nightTime{ d["nightTime"]=[String](); for z in y{d["nightTime"]!.append(z.rawValue)} }
+			if let y = a.pet{ d["pet"]=[String](); for z in y{d["pet"]!.append(z.rawValue)} }
+			if let y = a.mold{ d["mold"]=[String](); for z in y{d["mold"]!.append(z.rawValue)} }
+			if let y = a.dust{ d["dust"]=[String](); for z in y{d["dust"]!.append(z.rawValue)} }
+			if let y = a.outdoors{ d["outdoors"]=[String](); for z in y{d["outdoors"]!.append(z.rawValue)} }
+			if let y = a.energyLevel{ d["energyLevel"]=[String](); for z in y{d["energyLevel"]!.append(z.rawValue)} }
+			if let y = a.headache{ d["headache"]=[String](); for z in y{d["headache"]!.append(z.rawValue)} }
+		}
+		return d
 	}
+	
+	func setFromDictionary( d:[String:[String]] ) {
+		var a = Allergies()
+		for (key,value) in d{
+			switch key{
+			case "eyes": a.eyes=[]; for v in value{if let e=eyes_allergies.init(rawValue:v){a.eyes!.append(e)} }
+			case "sinus": a.sinus=[]; for v in value{if let e=sinus_allergies.init(rawValue:v){a.sinus!.append(e)} }
+			case "nose": a.nose=[]; for v in value{if let e=nose_allergies.init(rawValue:v){a.nose!.append(e)} }
+			case "throat": a.throat=[]; for v in value{if let e=throat_allergies.init(rawValue:v){a.throat!.append(e)} }
+			case "lungs": a.lungs=[]; for v in value{if let e=lungs_allergies.init(rawValue:v){a.lungs!.append(e)} }
+			case "skin": a.skin=[]; for v in value{if let e=skin_allergies.init(rawValue:v){a.skin!.append(e)} }
+			case "meds": a.meds=[]; for v in value{if let e=meds_allergies.init(rawValue:v){a.meds!.append(e)} }
+			case "wokeUp": a.wokeUp=[]; for v in value{if let e=wokeUp_allergies.init(rawValue:v){a.wokeUp!.append(e)} }
+			case "nightTime": a.nightTime=[]; for v in value{if let e=nightTime_allergies.init(rawValue:v){a.nightTime!.append(e)} }
+			case "pet": a.pet=[]; for v in value{if let e=pet_allergies.init(rawValue:v){a.pet!.append(e)} }
+			case "mold": a.mold=[]; for v in value{if let e=mold_allergies.init(rawValue:v){a.mold!.append(e)} }
+			case "dust": a.dust=[]; for v in value{if let e=dust_allergies.init(rawValue:v){a.dust!.append(e)} }
+			case "outdoors": a.outdoors=[]; for v in value{if let e=outdoors_allergies.init(rawValue:v){a.outdoors!.append(e)} }
+			case "energyLevel": a.energyLevel=[]; for v in value{if let e=energyLevel_allergies.init(rawValue:v){a.energyLevel!.append(e)} }
+			case "headache": a.headache=[]; for v in value{if let e=headache_allergies.init(rawValue:v){a.headache!.append(e)} }
+			default: break
+			}
+		}
+	}
+
+	func getDateString() -> String{
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd"
+		return formatter.string(from: self.date)
+	}
+
+	override init() {
+		// dates are initialized to NOW time, in user's time zone
+		self.date = Date()
+		super.init()
+	}
+
 
 	func log(){
 //		print(self.values)
